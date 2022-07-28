ba6a267c4359d541d9edab4018e9029e74ef4614
张鑫 <2916733574@qq.com>
2022-07-28 17:08:55 +0800

    生成ast树

--- packages/compiler-core/dist/compiler-core.global.js
+++ packages/compiler-core/dist/compiler-core.global.js
@@ -2,7 +2,21 @@ var VueCompilerCore = (() => {
   var __defProp = Object.defineProperty;
   var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
   var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __getOwnPropSymbols = Object.getOwnPropertySymbols;
   var __hasOwnProp = Object.prototype.hasOwnProperty;
+  var __propIsEnum = Object.prototype.propertyIsEnumerable;
+  var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+  var __spreadValues = (a, b) => {
+    for (var prop in b || (b = {}))
+      if (__hasOwnProp.call(b, prop))
+        __defNormalProp(a, prop, b[prop]);
+    if (__getOwnPropSymbols)
+      for (var prop of __getOwnPropSymbols(b)) {
+        if (__propIsEnum.call(b, prop))
+          __defNormalProp(a, prop, b[prop]);
+      }
+    return a;
+  };
   var __export = (target, all) => {
     for (var name in all)
       __defProp(target, name, { get: all[name], enumerable: true });
@@ -32,7 +46,11 @@ var VueCompilerCore = (() => {
     };
   }
   function isEnd(context) {
-    return !context.source;
+    const source = context.source;
+    if (source.startsWith("</")) {
+      return true;
+    }
+    return !source;
   }
   function getCursor(context) {
     let { line, column, offset } = context;
@@ -86,23 +104,129 @@ var VueCompilerCore = (() => {
       loc: getSelection(context, start)
     };
   }
+  function parseInterpolation(context) {
+    const start = getCursor(context);
+    const closeIndex = context.source.indexOf("}}", "{{".length);
+    advanceBy(context, 2);
+    const innerStart = getCursor(context);
+    const innerEnd = getCursor(context);
+    const rawContentLength = closeIndex - 2;
+    let preContent = parseTextData(context, rawContentLength);
+    let content = preContent.trim();
+    let startOffset = preContent.indexOf(content);
+    if (startOffset > 0) {
+      advancePositionWithMutation(innerStart, preContent, startOffset);
+    }
+    let endOffset = startOffset + content.length;
+    advancePositionWithMutation(innerEnd, preContent, endOffset);
+    advanceBy(context, 2);
+    return {
+      type: 5 /* INTERPOLATION */,
+      context: {
+        type: 4 /* SIMPLE_EXPRESSION */,
+        content,
+        loc: getSelection(context, innerStart, innerEnd)
+      },
+      loc: getSelection(context, start)
+    };
+  }
+  function advanceBySpaces(context) {
+    let match = /^[ \t\r\n]+/.exec(context.source);
+    if (match) {
+      advanceBy(context, match[0].length);
+    }
+  }
+  function parseAttributeValue(context) {
+    const start = getCursor(context);
+    let quote = context.source[0];
+    let content;
+    if (quote == '"' || quote == "'") {
+      advanceBy(context, 1);
+      const endIndex = context.source.indexOf(quote);
+      content = parseTextData(context, endIndex);
+      advanceBy(context, 1);
+    }
+    return {
+      content,
+      loc: getSelection(context, start)
+    };
+  }
+  function parseAttribute(context) {
+    const start = getCursor(context);
+    const match = /^[^\t\r\n\f />][^\t\r\n\f />=]*/.exec(context.source);
+    let name = match[0];
+    advanceBy(context, name.length);
+    advanceBySpaces(context);
+    advanceBy(context, 1);
+    advanceBySpaces(context);
+    let value = parseAttributeValue(context);
+    return {
+      type: 6 /* ATTRIBUTE */,
+      name,
+      value: __spreadValues({
+        type: 2 /* TEXT */
+      }, value),
+      loc: getSelection(context, start)
+    };
+  }
+  function parseAttributes(context) {
+    const props = [];
+    const source = context.source;
+    while (source.length > 0 && !(source.startsWith(">") || source.startsWith("/>"))) {
+      const prop = parseAttribute(context);
+      props.push(prop);
+      advanceBySpaces(context);
+    }
+    return props;
+  }
+  function parseTag(context) {
+    const start = getCursor(context);
+    const match = /^<\/?([a-z][^\t\r\n\f />]*)/i.exec(context.source);
+    const tag = match[1];
+    advanceBy(context, match[0].length);
+    advanceBySpaces(context);
+    let props = parseAttributes(context);
+    let isSelfClosing = context.source.startsWith("/>");
+    advanceBy(context, isSelfClosing ? 2 : 1);
+    return {
+      type: 1 /* ELEMENT */,
+      tag,
+      isSelfClosing,
+      children: [],
+      props,
+      loc: getSelection(context, start)
+    };
+  }
+  function parseElement(context) {
+    let ele = parseTag(context);
+    let children = parseChildren(context);
+    if (context.source.startsWith("</")) {
+      parseTag(context);
+    }
+    ele.loc = getSelection(context, ele.loc.start);
+    ele.children = children;
+    return ele;
+  }
   function parse(template) {
     const context = createParserContext(template);
+    return parseChildren(context);
+  }
+  function parseChildren(context) {
     const nodes = [];
     while (!isEnd(context)) {
       const source = context.source;
       let node;
       if (source.startsWith("{{")) {
+        node = parseInterpolation(context);
       } else if (source.startsWith("<")) {
+        node = parseElement(context);
       }
       if (!node) {
         node = parseText(context);
       }
       nodes.push(node);
-      break;
     }
-    console.log(nodes);
-    return context;
+    return nodes;
   }
   function compile(template) {
     const ast = parse(template);
--- packages/compiler-core/src/index.ts
+++ packages/compiler-core/src/index.ts
@@ -12,7 +12,11 @@ function createParserContext(template){
 
 function isEnd(context){
   // 如果解析完毕后为空字符串 表示解析完毕
-  return !context.source
+  const source = context.source
+  if(source.startsWith('</')){
+    return true
+  }
+  return !source
 }
 
 function getCursor(context){
@@ -68,7 +72,6 @@ function parseText(context){
   }
 
   // 创建 行列信息
-
   const start = getCursor(context) //开始位置
   // 取内容  advancePositionWithMutation再获取结束位置
   const content = parseTextData(context,endIndex)
@@ -78,13 +81,158 @@ function parseText(context){
     content:content,
     loc:getSelection(context,start)
   } 
+}
+
+// 处理插值表达式
+function parseInterpolation(context){
+  const start = getCursor(context)
+  // 查找结束的大括号
+  const closeIndex = context.source.indexOf("}}","{{".length)
+  advanceBy(context,2)  //删掉 {{
+
+  const innerStart = getCursor(context)
+  const innerEnd = getCursor(context)
   
+  // 拿到大括号中内容的长度
+  const rawContentLength = closeIndex - 2;
+  // 拿到文本内容   且更新了行列 偏移量信息
+  let preContent = parseTextData(context,rawContentLength)
+  let content = preContent.trim()
+
+  let startOffset = preContent.indexOf(content)
+
+  if(startOffset > 0 ){
+    advancePositionWithMutation(innerStart,preContent,startOffset)
+  }
+  
+  let endOffset = startOffset + content.length
+
+  advancePositionWithMutation(innerEnd,preContent,endOffset)
+
+  advanceBy(context,2)  //删掉 }}
+
+  return {
+    type:NodeTypes.INTERPOLATION,
+    context:{
+      type:NodeTypes.SIMPLE_EXPRESSION,
+      content,
+      loc:getSelection(context,innerStart,innerEnd)
+    },
+    loc:getSelection(context,start)
+  }
+}
+
+// 删空格 <div     >
+function advanceBySpaces(context){
+  let match = /^[ \t\r\n]+/.exec(context.source)
+  if(match){
+      advanceBy(context,match[0].length) 
+  }
+}
+function parseAttributeValue(context){
+    const start = getCursor(context)
+    let quote = context.source[0]
+    let content;
+    // 判断是单引号还是双引号  还有可能没有 先不处理
+    if(quote == '"' || quote == "'"){
+      advanceBy(context,1)
+      const endIndex = context.source.indexOf(quote)
+      content = parseTextData(context,endIndex)
+      advanceBy(context,1)
+    }
+
+    return {
+      content,
+      loc:getSelection(context,start)
+    }
+    
+}
+
+function parseAttribute(context){
+  const start = getCursor(context)
+
+  // 属性的名字
+  const match = /^[^\t\r\n\f />][^\t\r\n\f />=]*/.exec(context.source)
+  let name = match[0]
+  advanceBy(context,name.length)
+  advanceBySpaces(context) //删空格
+  advanceBy(context,1)  //删 等于号 =
+  advanceBySpaces(context) //删空格
+  let value = parseAttributeValue(context)
+
+  
+  return {
+    type:NodeTypes.ATTRIBUTE,
+    name,
+    value:{
+      type:NodeTypes.TEXT,
+      ...value
+    },
+    loc:getSelection(context,start)
+  }
+
+}
+// 处理属性
+function parseAttributes(context){
+  const props = []
+  const source = context.source
+  while(source.length > 0 && !(source.startsWith('>') || source.startsWith('/>'))){
+    const prop = parseAttribute(context)
+    props.push(prop)
+    advanceBySpaces(context)
+  }
+  return props
+
+}
+function parseTag(context){
+  const start = getCursor(context);
+  const match = /^<\/?([a-z][^\t\r\n\f />]*)/i.exec(context.source);
+  const tag = match[1]  //标签名
+  // console.log(tag);
+  advanceBy(context,match[0].length)  //删除标签 <div
+  advanceBySpaces(context)   // 删空格 <div     >
+  // 处理属性
+  let props = parseAttributes(context)
+  // 判断是不是一个自闭和标签
+  
+  let isSelfClosing = context.source.startsWith("/>")
+
+  advanceBy(context,isSelfClosing?2:1)   //删除 /> 或者 >
+
+  return {
+    type:NodeTypes.ELEMENT,
+    tag,
+    isSelfClosing,
+    children:[],
+    props,
+    loc:getSelection(context,start)
+  }
+
+}
+// 处理标签
+function parseElement(context){
+  // <div>
+ let ele = parseTag(context)
+  // div中间的部分
+  let children = parseChildren(context)
+  //  </div>
+ if(context.source.startsWith("</")){
+    parseTag(context)
+  }
+  ele.loc = getSelection(context,ele.loc.start)  //计算最新的位置信息
+  ele.children = children
+  return ele
 }
 
 function parse(template){
   // 创建一个解析的上下文 
   const context = createParserContext(template)
 
+  return parseChildren(context)
+  
+}
+
+function parseChildren(context){
   //  <  元素
   //  {{}} 表达式
   //  其他 文本 注释等
@@ -93,23 +241,18 @@ function parse(template){
     const source  = context.source
     let node;
     if(source.startsWith("{{")){
-      
+      node = parseInterpolation(context)
     }else if(source.startsWith("<")){
-
+      node = parseElement(context)
     }
-
+    
     // 文本
     if(!node){
       node = parseText(context)
-      
     }
     nodes.push(node)
-    break;
   }
-  console.log(nodes);
-  
-
-  return context
+  return nodes
 }
 
 
