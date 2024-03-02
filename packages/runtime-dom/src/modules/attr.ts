4f7dc82bc184061b3a7ef73029704ec2a5f41e32
zhangxin <2916733574@qq.com>
2024-03-02 16:22:20 +0800

    init

--- packages/runtime-dom/src/modules/attr.ts
+++
@@ -1,7 +0,0 @@
-export function patchAttr(el,key,nextValue){
-  if(nextValue){
-    el.setAttribute(key,nextValue)
-  }else{
-    el.removeAttribute(key)
-  }
-}
--- packages/runtime-dom/src/modules/class.ts
+++
@@ -1,8 +0,0 @@
-export function patchClass(el,nextValue){
-  if(nextValue){
-     el.className = nextValue
-  }else{
-   // 如果新的class是空的 就删掉class
-    el.removeattribute("class")
-  }
-}
